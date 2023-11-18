cde5d1912f7ea4983fcee725a2888bf892dd69ad
minyou73 <145949811+minyou73@users.noreply.github.com>
2023-11-18 20:56:53 +0900

    프로그래머스 입문

--- programmers/삼각형의완성조건(2)/sol.py
+++ programmers/삼각형의완성조건(2)/sol.py
@@ -2,3 +2,34 @@
 # 삼각형의 두 변의 길이가 담긴 배열 sides이 매개변수로 주어집니다.
 # 나머지 한 변이 될 수 있는 정수의 개수를 return하도록 solution 함수를 완성해주세요.
 
+# [3, 6]  6-3+1 <= 456/78  < 6+3
+# [7, 11]  11-7+1  <=  5,6,7,8,9,10,11/12,13,14,15,16,17  < 7+11
+
+
+
+def solution(sides):
+    sides = sorted(sides, reverse=False) #[3, 6]
+    a = sides[0]
+    b = sides[-1]
+    answer = []
+    for i in range((b - a + 1), (a + b) ):
+        answer.append(i)
+    return len(answer)
+
+
+print(solution([3, 6]))
+print(solution([11, 7]))
+
+
+####################################################################
+
+def solution(sides):
+    answer = []
+    answer2 = []
+    mn = min(sides)
+    mx = max(sides)
+    for a in range(mx-mn+1, mx+1):
+        answer.append(a)
+    for b in range(mx+1, mx+mn):
+        answer2.append(b)
+    return len(answer)+len(answer2)
--- programmers/외계어사전/sol.py
+++ programmers/외계어사전/sol.py
@@ -2,10 +2,53 @@
 # spell에 담긴 알파벳을 한번씩만 모두 사용한 단어가 dic에 존재한다면 1,
 #  존재하지 않는다면 2를 return하도록 solution 함수를 완성해주세요.
 
+def solution(spell, dic):
+    diction = {}
+
+    for k, v in enumerate(dic, 1):
+        count = 0
+        for s in spell:
+            count += v.count(s)
+        diction[v] = count
+
+    return diction
+
+print(solution(["z", "d", "x"], ["def", "dww", "dzx", "loveaw"]))
+
+
+
+
+# def solution(spell, dic):
+#     count = 0
+#     diction = {d:0 for d in dic}  #{'def': 0, 'dww': 0, 'dzx': 0, 'loveaw': 0}
+
+#     for i in range(len(spell)):   # spell [z,d,x]
+#         for j in diction:         # diction에 있는 def, dww, dzx, loveaw 가져오기
+#             if spell[i] in j:     # spell [z,d,x]가 def에 있다면, dww에 있다면
+#                 diction[j] += 1   # diction의 value값 올려줌 하니씩
+    
+#     for k, v in diction.items():  # key, value 값 동시비교
+#         if v == len(spell):       # value 가 한번씩 다 사용했다면
+#             return 1
+#     else:
+#         return 2
+
+# print(solution(["z", "d", "x"],["def", "dww", "dzx", "loveaw"]))
+# # print(solution(["s", "o", "m", "d"], ["moos", "dzx", "smm", "sunmmo", "som"]))
+
+
+########################################################################
+
+#....?? 
+# ....ㅎ
+# 어...이...가 .. 없네..?
 
 def solution(spell, dic):
-    
-    for i in range(len(dict)):
-        dict[i].split()
+    for d in dic:
+        if not set(spell) - set(d):
+            return 1
+    return 2
+# print(solution(["z", "d", "x"],["def", "dww", "dzx", "loveaw"]))
 
-print(solution(["p", "o", "s"], ["sod", "eocd", "qixm", "adio", "soo"]))
+####################################################################
+
