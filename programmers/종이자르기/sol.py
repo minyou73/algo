b14b8c870a677bff3bfefe65dea8d1349a414bea
minyou73 <145949811+minyou73@users.noreply.github.com>
2023-11-25 22:53:00 +0900

    프로그래머스 입문

--- programmers/종이자르기/sol.py
+++ programmers/종이자르기/sol.py
@@ -1,7 +1,21 @@
 # 머쓱이는 큰 종이를 1 x 1 크기로 자르려고 합니다. 예를 들어 2 x 2 크기의 종이를 1 x 1 크기로 자르려면 
 # 최소 가위질 세 번이 필요합니다. 최소로 가위질 해야하는 횟수를 return 
 
-def solution(M, N):
-    M / 
+def solution(width, length):
+    w = width - 1
+    l = length - 1
+
+    if width == 1 and length == 1:
+        return 0
+    elif width == 1:
+        return l
+    elif length == 1:
+        return w
+    else:
+        return (w * length) + l
 
 print(solution(2,2))
+
+##############################################################
+def solution(M, N):
+    return (M * N) - 1
--- programmers/캐릭터의좌표/sol.py
+++ programmers/캐릭터의좌표/sol.py
@@ -15,16 +15,18 @@ def solution(keyinput, board):
     center = [0,0]
     
     for i in keyinput:
-        if i == 'up' and center[1] <= board[1]:
-            center[1] = center[1]+1
-        elif i == 'down' and center[1]:
-            center[1] = center[1]-1
-        elif i == 'left':
-            center[0] = center[0]-1
-        elif i == 'right':
-            center[0] = center[0]+1
+        if i == 'up' and center[1] < int(board[1]/2):
+            center[1] += 1
+        elif i == 'down' and center[1] > -int(board[1]/2) :
+            center[1] -= 1
+        elif i == 'left' and center[0] > -int(board[0]/2):
+            center[0] -= 1
+        elif i == 'right'and center[0] < int(board[0]/2):
+            center[0] += 1
     
     return center
 
 
 print(solution(["left", "right", "up", "right", "right"],[11, 11]))
+
+print(solution(["down", "down", "down", "down", "down"], [7,9]))
